98feea3ebd347343a17e404236a970326d0142a9
MINE <arkaan.smait7@gmail.com>
2016-03-16 14:55:16 +0700

    membuat class Bingkai.java

---
+++ src/Bingkai.java
@@ -0,0 +1,7 @@
+public class Bingkai{
+	private double panjang, lebar;
+	private String[] jenisBingkai = {"Hitam Metalik", "Coklat Metalik", "Coklat Bercorak",
+									 "Hitam Bercorak", "Emas", "Perak"};
+	private int[] hargaPerM = {3000, 2700, 2300, 2500, 5000, 4000};
+	
+}
