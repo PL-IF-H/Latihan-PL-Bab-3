edcb1b6a461950ce7fc286e954925d117a784026
Indah WA <Indah WA>
2016-03-16 16:59:39 +0700

    add 5

--- src/Bingkai.java
+++ src/Bingkai.java
@@ -14,6 +14,13 @@ public class Bingkai {
         System.out.println("Hitam Metalik 	||\t3000\nCoklat Metalik 	||\t2700\nCoklat Bercorak ||\t2300\nHitam Bercorak 	||\t2500\nEmas 		||\t5000\nPerak 		||\t4000");
         System.out.println("==================================\n");
 
+	System.out.print("Masukkan ID         : ");
+        String id = input.nextLine();
+        System.out.print("Masukkan Nama       : ");
+        String nama = input.nextLine();
+        System.out.print("Masukkan Tanggal    : ");
+        String tgl = input.nextLine();
+        data.dataPemesanan(id, nama, tgl);
 
     }
 }
