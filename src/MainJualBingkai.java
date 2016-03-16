f3dc2afa6ac0eccb1a870f457d5ea8ce9aa3139d
Diva Candra <Diva Candra>
2016-03-16 21:58:10 +0700

    Tugas3

--- src/MainJualBingkai.java
+++ src/MainJualBingkai.java
@@ -1,21 +1,38 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package pemlan.tugas3;
-
-/**
- *
- * @author divac
- */
-public class PemlanTugas3 {
-
-    /**
-     * @param args the command line arguments
-     */
+import java.util.Scanner ;
+public class MainJualBingkai {
     public static void main(String[] args) {
-        // TODO code application logic here
-    }
-    
+        Scanner input = new Scanner(System.in);
+        Scanner in = new Scanner(System.in);
+        String Nama, ID, Jenis, Tanggal;
+        double P, L ;
+        DagangBingkai jual = new DagangBingkai();
+        System.out.print("Masukan Nama                   : ");
+        Nama = input.nextLine();
+        System.out.print("Masukan ID                     : ");
+        ID = input.nextLine();
+        System.out.print("Masukan Tanggal Pemesanan      : ");
+        Tanggal = input.nextLine();
+        System.out.print("Masukan Panjang Bingkai        : ");
+        P = in.nextInt();
+        jual.setPanjang(P);
+        System.out.print("Masukan Lebar Bingkai          : ");
+        L = in.nextInt();
+        jual.setLebar(L);
+        System.out.println("=================||=======================");
+        System.out.println("||     Kode      ||   Nama Barang       ||");
+        System.out.println("==========================================");
+        System.out.println("||      HM       ||   Hitam Metalik     ||");
+        System.out.println("||      CM       ||   Coklat Metalik    ||");
+        System.out.println("||      CB       ||   Coklat Bercorak   ||");
+        System.out.println("||      HB       ||   Hitam Bercorak    ||");
+        System.out.println("||      E        ||   Emas              ||");
+        System.out.println("||      P        ||   Perak             ||");
+        System.out.println("=================||=======================");
+        System.out.print("Masukan Kode Jenis Barang      : ");
+        Jenis = input.nextLine();
+        jual.hitungHarga(Jenis);
+        jual.inputPesan(ID, Nama, Tanggal, Jenis);
+        jual.displayF();
+    }   
 }
