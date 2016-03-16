0a8c73c8832e615c54eaaacc7cac4899ceeda326
ismajannah <ismajannah999@gmail.com>
2016-03-16 22:29:48 +0700

    Update Pemesan.java

--- src/Pemesan/Pemesan.java
+++ src/Pemesan/Pemesan.java
@@ -1,16 +1,16 @@
 
 package Pemesan;
 public class Pemesan {
-    String id, nama, tanggal, jenis;
-    int panjang, lebar;
+    String id, nama, tanggal, jenis;//fields
+    int panjang, lebar;//fields
     
-    public void setPemesan (String id, String nama, String tanggal){
+    public void setPemesan (String id, String nama, String tanggal){ //method
         id = id;
         nama = nama;
         tanggal = tanggal;
 
     }
-    public void displayPemesan(String nama, String tanggal){
+    public void displayPemesan(String nama, String tanggal){ //method
         System.out.println("Pemesan             : "+nama);
         System.out.println("Tanggal             : "+tanggal);
     }
