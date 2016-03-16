65ec01b62d3713c6a8f6b5420643aa84d6fb23ea
Indah WA <Indah WA>
2016-03-16 16:56:09 +0700

    add 4

--- src/Bingkai.java
+++ src/Bingkai.java
@@ -3,9 +3,17 @@ package Tugas;
 //objek     : data, data2
 //atribut   : ID, nama, tanggal, jenis, panjang, lebar
 //method    : hitungUkuran
-
+import java.util.Scanner;
 public class Bingkai {
     public static void main(String[] args) {
-    
+    Scanner input = new Scanner (System.in);
+    Pemesanan data = new Pemesanan("","","");
+    Pemesanan data2 = new Pemesanan(0, 0);
+        System.out.println("=========BINGKAI LUKISAN==========");
+        System.out.println("JENIS BINGKAI   || HARGA PER METER\n==================================");
+        System.out.println("Hitam Metalik 	||\t3000\nCoklat Metalik 	||\t2700\nCoklat Bercorak ||\t2300\nHitam Bercorak 	||\t2500\nEmas 		||\t5000\nPerak 		||\t4000");
+        System.out.println("==================================\n");
+
+
     }
 }
