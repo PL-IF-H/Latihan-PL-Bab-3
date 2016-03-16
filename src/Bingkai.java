ea26c220044e33c5906caa6394b88a95908c1512
MINE <arkaan.smait7@gmail.com>
2016-03-16 16:08:20 +0700

    tambah method hitungDiskon

--- src/Bingkai.java
+++ src/Bingkai.java
@@ -2,10 +2,14 @@ public class Bingkai{
 	private double panjang, lebar;
 	private String jenis;
 	private String jenisBingkai = "1. Hitam Metalik\n"+"2. Coklat Metalik\n"+"3. Coklat Bercorak\n"+"4. Hitam Bercorak\n"+"5. Emas\n"+"6. Perak\n";
-	private int[] hargaPerM = {3000, 2700, 2300, 2500, 5000, 4000};
+	private double[] hargaPerM = {3000, 2700, 2300, 2500, 5000, 4000};
+	private double harga, total, diskon;
 	
 	public void setJenis(String j){
 		jenis = j;
+	}
+	public String getJenis(){
+		return jenis;
 	}
 	public void setPanjang(double p){
 		panjang = p;
@@ -15,5 +19,22 @@ public class Bingkai{
 	}
 	public String displayBingkai(){
 		return jenisBingkai;
+	}
+	public double hitungDiskon(){
+		if(jenis.equals("Hitam Metalik")||jenis.equals("Emas")||jenis.equals("Perak")){
+			if(harga>300000){
+				diskon = 0.5*harga;
+			}
+			else if(harga>450000){
+				diskon = 0.1*harga;
+			}
+			else if(harga>600000){
+				diskon = 1.5*harga;
+			}
+		}
+		else{
+			diskon = 0;
+		}
+		return diskon;
 	}
 }
