a5d110676d20f88b587cb52b59d5f5905bb922ad
MINE <arkaan.smait7@gmail.com>
2016-03-16 15:05:45 +0700

    membuat main class

---
+++ src/MainBingkai.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+public class MainBingkai{
+	public static void main(String[] args){
+		Scanner in = new Scanner(System.in);
+		System.out.println("===Toko Adit===");
+		
+	}
+}
