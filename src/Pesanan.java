adf8c77ad1bd959bca433228a2a1a4410485567f
Firman Budi Safrizal <firmanbudis@gmail.com>
2016-03-16 19:24:35 +0700

    tambah method di kelas pesanan

--- src/Pesanan.java
+++ src/Pesanan.java
@@ -4,4 +4,16 @@ public class Pesanan {
     private String jenis_bingkai;
     private int harga_bingkai;
     private double luas;
+    public void setJenis_bingkai(String jenis){
+        this.jenis_bingkai = jenis;
+    }
+
+    public String getJenis_bingkai() {
+        return jenis_bingkai;
+    }
+  
+    public double hitungLuas(double panjang, double lebar){
+        this.luas = panjang * lebar;
+        return luas;
+    }
 }
