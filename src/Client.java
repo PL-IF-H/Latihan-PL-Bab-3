f2d1e9389d69cf1cbab9147736c181999f86bd62
Firman Budi Safrizal <firmanbudis@gmail.com>
2016-03-16 19:17:23 +0700

    tambah atribut di kelas client dan pesanan

--- src/Client.java
+++ src/Client.java
@@ -1,5 +1,5 @@
 package tugasprakbab3;
 
 public class Client {
-    
+    private String ID_Pesanan,nama_pemesan,tanggal;
 }
--- src/Pesanan.java
+++ src/Pesanan.java
@@ -1,5 +1,7 @@
 package tugasprakbab3;
 
 public class Pesanan {
-    
+    private String jenis_bingkai;
+    private int harga_bingkai;
+    private double luas;
 }
