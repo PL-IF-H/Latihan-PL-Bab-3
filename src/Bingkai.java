242143e079d52743dda876202c5490821f002f1c
Indah WA <Indah WA>
2016-03-16 16:44:24 +0700

    add 1

---
+++ src/Bingkai.java
@@ -0,0 +1,11 @@
+package Tugas;
+//class     : Pemesanan, Bingkai
+//objek     : data, data2
+//atribut   : ID, nama, tanggal, jenis, panjang, lebar
+//method    : hitungUkuran
+
+public class Bingkai {
+    public static void main(String[] args) {
+    
+    }
+}
---
+++ src/Pemesanan.java
@@ -0,0 +1,13 @@
+package Tugas;
+public class Pemesanan {
+	String ID, nama, tanggal, jenis;
+	int panjang, lebar;
+    	public Pemesanan (String id, String nm, String tgl){
+        ID = id;
+        nama = nm;
+        tanggal = tgl;
+    }
+
+
+
+}
