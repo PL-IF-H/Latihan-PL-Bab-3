7ab3101f2bcb4248144bb38a5570e086e4248dda
Firman Budi Safrizal <firmanbudis@gmail.com>
2016-03-16 19:20:05 +0700

    tambah constructor dan method di kelas client

--- src/Client.java
+++ src/Client.java
@@ -2,4 +2,26 @@ package tugasprakbab3;
 
 public class Client {
     private String ID_Pesanan,nama_pemesan,tanggal;
+    public Client(){
+        ID_Pesanan = "";
+        nama_pemesan = "";
+        tanggal = "";
+    }
+    public Client(String id, String nama, String tgl){
+        ID_Pesanan = id;
+        nama_pemesan = nama;
+        tanggal = tgl;
+    }
+
+    public String getID_Pesanan() {
+        return ID_Pesanan;
+    }
+
+    public String getNama_pemesan() {
+        return nama_pemesan;
+    }
+
+    public String getTanggal() {
+        return tanggal;
+    }
 }
