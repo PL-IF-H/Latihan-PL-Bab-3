4d9d435c6c37fa23456348083102fa51d8fb3979
Iskarimah <el_rikma@yahoo.co.id>
2016-03-17 00:24:30 +0700

    Added files via upload

---
+++ src/diskon.java
@@ -0,0 +1,34 @@
+package tgs3;
+
+import java.util.Scanner;
+
+public class diskon {
+    Scanner input = new Scanner (System.in);
+    private int p;
+    private int l;
+    private String hitamMetalik;
+    private String coklatMetalik;
+    private String coklatBercorak;
+    private String hitamBercorak;
+    private String emas;
+    private String perak;
+    
+    Pemesanan a = new Pemesanan ();
+    //overloading construktor
+    public diskon (String hitamMrtalik, String coklatMetalik){
+        this.hitamMetalik=hitamMetalik;
+        this.coklatMetalik=coklatMetalik;
+    }
+    //overloading construktor
+    public diskon() {
+        this.p=p;
+        this.l=l;
+        this.hitamMetalik=hitamMetalik;
+        this.coklatBercorak=coklatBercorak;
+        this.coklatMetalik=coklatMetalik;
+        this.hitamBercorak=hitamBercorak;
+        this.emas=emas;
+        this.perak=perak;
+       
+    }
+}
